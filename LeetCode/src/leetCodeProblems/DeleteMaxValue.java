b3ae4ff89190100eaf3a84d7dd232865333cb90e
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-03-24 21:16:42 +0530

    commit

---
+++ LeetCode/src/leetCodeProblems/DeleteMaxValue.java
@@ -0,0 +1,45 @@
+package leetCodeProblems;
+
+import java.util.Arrays;
+
+public class DeleteMaxValue {
+
+	public static void main(String[] args) {
+
+		long sum = 0;
+		
+		int arr[][] = { { 1, 2, 4 }, { 3, 3, 1 },{4,5,6} };
+		int n = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+
+			}
+			System.out.println();
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			Arrays.sort(arr[i]);
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+
+			}
+			System.out.println();
+		}
+		int max=0;
+		for(int i=0;i<arr[0].length;i++) {
+			max=0;
+			for(int j=0;j<arr.length;j++) {
+				max=Math.max(arr[j][i],max);
+			}
+			System.out.println(max);
+			sum+=max;
+		}
+		System.out.println(sum);
+
+	}
+
+}
