70e8fbd961332e6db746969d00d9aa41ec8da371
Ujjwal Kumar <99142201+ujjwalkr01@users.noreply.github.com>
2023-06-22 00:25:27 +0530

    commit

---
+++ LeetCode/src/leetCodeProblems/RomanToInteger.java
@@ -0,0 +1,38 @@
+package leetCodeProblems;
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+	public static void main(String[] args) {
+
+		String s = "DCXXI";
+
+		convertRomanTOInteger(s);
+	}
+
+	private static void convertRomanTOInteger(String s) {
+		int sum = 0, count = 1;
+		boolean isRepeating = false;
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+
+		for (int i = 0; i < s.length(); i++) {
+			if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+				sum -= map.get(s.charAt(i));
+			} else {
+				sum += map.get(s.charAt(i));
+			}
+		}
+		System.out.println(sum);
+
+	}
+
+}
